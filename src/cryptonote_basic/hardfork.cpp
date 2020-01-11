0e57e17c4be3ac552d671dcbdf746c94d1889a75
worktipsdev <48468699+worktipsdev@users.noreply.github.com>
2020-01-12 01:27:41 +0530

    Hardforkssss

--- src/cryptonote_basic/hardfork.cpp
+++ src/cryptonote_basic/hardfork.cpp
@@ -65,27 +65,27 @@ static uint8_t get_block_version(const cryptonote::block &b)
 // version 7 from the start of the blockchain, inhereted from Monero mainnet
 static constexpr HardFork::Params mainnet_hard_forks[] =
 {
-  { network_version_7,                      1,      0, 1503046577 },
-  { network_version_8,                      64324,  0, 1533006000 },
-  { network_version_9_service_nodes,        101250, 0, 1537444800 },
-  { network_version_10_bulletproofs,        161849, 0, 1544743800 }, // 2018-12-13 23:30UTC
-  { network_version_11_infinite_staking,    234767, 0, 1554170400 }, // 2019-03-26 13:00AEDT
-  { network_version_12_checkpointing,       321467, 0, 1563940800 }, // 2019-07-24 14:00AEDT
-  { network_version_13_enforce_checkpoints, 385824, 0, 1571850000 }, // 2019-10-23 19:00AEDT
+  { network_version_7,                		  	 1, 0, 1554448799 },
+  { network_version_8,               		   	10, 0, 1554448860 },
+  { network_version_9_service_nodes,		 54689, 0, 1561804620 },
+  { network_version_10_bulletproofs,		 54690, 0, 1561804740 },
+  { network_version_11_infinite_staking,	 54691, 0, 1561804860 }, // 2019-03-26 13:00AEDT
+  { network_version_12_checkpointing,       167000, 0, 1563940800 }, // 2019-07-24 14:00AEDT
+  { network_version_13_enforce_checkpoints, 167010, 0, 1571850000 }, // 2019-10-23 19:00AEDT
   // TODO: add v14 fork height; also remember to update hf_min_worktips_versions in service_node_list
   // with the final 6.1.0 release version.
 };
 
 static constexpr HardFork::Params testnet_hard_forks[] =
 {
-  { network_version_7,                      1,      0, 1533631121 },
-  { network_version_8,                      2,      0, 1533631122 },
-  { network_version_9_service_nodes,        3,      0, 1533631123 },
-  { network_version_10_bulletproofs,        4,      0, 1542681077 },
-  { network_version_11_infinite_staking,    5,      0, 1551223964 },
-  { network_version_12_checkpointing,       75471,  0, 1561608000 }, // 2019-06-28 14:00AEDT
-  { network_version_13_enforce_checkpoints, 127028, 0, 1568440800 }, // 2019-09-13 16:00AEDT
-  { network_version_14_blink_lns,           174630, 0, 1575075600 }, // 2019-11-30 07:00UTC
+  { network_version_7,                   1, 0, 1554448799 },
+  { network_version_8,                   2, 0, 1554448860 },
+  { network_version_9_service_nodes,     3, 0, 1560721200 },
+  { network_version_10_bulletproofs,     4, 0, 1560722200 },
+  { network_version_11_infinite_staking, 5, 0, 1560723200 },
+  { network_version_12_checkpointing,       50,  0, 1561608000 }, // 2019-06-28 14:00AEDT
+  { network_version_13_enforce_checkpoints, 60, 0, 1568440800 }, // 2019-09-13 16:00AEDT
+  { network_version_14_blink_lns,           70, 0, 1575075600 }, // 2019-11-30 07:00UTC
 };
 
 static constexpr HardFork::Params stagenet_hard_forks[] =
